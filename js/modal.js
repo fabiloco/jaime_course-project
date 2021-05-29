22216a52b32843bf2b93094554e7ec49fce57b2d
Fabian Sanchez <faalsaru@gmail.com>
2021-05-28 22:19:09 -0500

    final

--- js/modal.js
+++ js/modal.js
@@ -6,12 +6,13 @@ document.addEventListener('DOMContentLoaded', () =>{
         btn.addEventListener('click', ()=>{
             if(btn.classList.contains('img-play1')) {
                 Swal.fire({
-                    html: `<iframe width="840" height="480" src="https://www.youtube.com/embed/wycJcr0knjI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+                    html: `<iframe width="840" height="480" src="https://www.youtube.com/embed/yaqvkwifFWs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
                     width: 896,
+                    showConfirmButton: false,
                 })
             } else if(btn.classList.contains('img-play2')) {
                 Swal.fire({
-                    html: `<iframe width="840" height="480" src="https://www.youtube.com/embed/yaqvkwifFWs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+                    html: `<iframe width="840" height="480" src="https://www.youtube.com/embed/wycJcr0knjI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
                     width: 896,
                     showConfirmButton: false,
                 })
